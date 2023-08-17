9807442e00c9068c416045c48665903fbd4cc737
saswat <saswatr.mohanty@hyscaler.com>
2023-08-17 11:44:42 +0530

    show product

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/products')
+      .then(response => response.json())
+      .then(data => {
+        setProducts(data.products);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div className="grid grid-cols-4 gap-4 p-4">
+      {products.map(product => (
+        <div key={product.id} className="bg-white p-4 shadow-md">
+        <div>
+        <img src= {product.image} alt="" />
+       
+        </div>
+          <h3 className="text-lg font-semibold">{product.name}</h3>
+          <p className="text-gray-600">{product.description}</p>
+          <div className="mt-2 text-blue-500">Price: ₹ {product.price}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+import logo from '../assets/logo.png'
+
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-500 p-4 flex justify-between">
-        <div className="flex items-center">
-        <a href="/" className="text-white text-xl font-semibold">
-          Home
-        </a>
+    <nav className="bg-blue-500 p-4 flex justify-between items-center">
+    <div className="flex items-center">
+    
+      <img src={logo} alt="logo" className='w-16' />
       </div>
 
-      <div className="flex space-x-4">
-        <a href="/login" className="text-white">
-          Login
-        </a>
-        <a href="/register" className="text-white">
-          Register
-        </a>
-        <button className="text-white">Logout</button>
+      <div className="flex items-center bg-white rounded-lg shadow-md px-2 py-1">
+        <input
+          type="text"
+          placeholder="Search for products, brands, and more"
+          className="w-full bg-transparent focus:outline-none"
+        />
+        <button className="text-blue-500">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </div>
+
+       <div className="flex space-x-4">
+        <div className="relative">
+          <FontAwesomeIcon icon={faShoppingCart} className="text-white text-lg cursor-pointer" />
+          <span className="absolute top-0 left-2 bg-red-500 rounded-full text-white px-1 text-xs">3+</span>
+        </div>
+        <div className="text-white text-sm cursor-pointer">Login</div>
       </div>
     </nav>
   );
