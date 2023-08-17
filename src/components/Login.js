06136a013e018a773971a387e62838b0ed4a9ce2
saswat <saswatr.mohanty@hyscaler.com>
2023-08-17 17:44:35 +0530

    updated register and login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
-const port = 8000
-
 
 const Login = () => {
   const [login, setLogin] = useState({
@@ -11,7 +9,8 @@ const Login = () => {
     password: "",
   });
 
-  const [showPassword] = useState(false);   const handleChange = (e) => {
+  const [showPassword] = useState(false);
+  const handleChange = (e) => {
     const { name, value } = e.target;
     setLogin((prev) => {
       return {
@@ -53,14 +52,14 @@ const Login = () => {
       return false;
     }
 
-    const response = await fetch(`http://localhost:${port}/api/login`, {
+    const response = await fetch(`http://127.0.0.1:8000/api/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
     });
     let data = await response.json();
-console.log(data)
-    const { msg, token,userId } = data;
+    console.log(data);
+    const { msg, token, userId } = data;
     console.log("token :", token);
     console.log(msg);
     if (msg === "invalid credentials") {
@@ -98,11 +97,11 @@ console.log(data)
         progress: undefined,
         theme: "colored",
       });
- 
+
       localStorage.setItem("token", token);
-      localStorage.setItem("userId",userId)
+      localStorage.setItem("userId", userId);
       console.log("logged in successfully");
-      navigate("/Property");
+      navigate("/");
     }
   };
 
@@ -114,71 +113,76 @@ console.log(data)
 
   return (
     <>
-    <div className="bg-gray-400 flex justify-center items-center min-h-screen ">
-    <div className="bg-gray-50 p-3 border rounded-xl shadow-xl max-w-3xl w-full sm:w-11/12 md:w-9/12 lg:w-8/12">
-      <h1 className="text-3xl text-blue-700 font-bold text-center">
-        Login Here
-      </h1>
-      <div className="flex flex-col md:flex-row">
-        <div className="w-full  px-5 mt-5">
-          <form className="flex flex-col gap-1">
-            <label htmlFor="email" className="ml-2">
-              Email
-            </label>
-            <input
-              type="email"
-              className="p-2 border rounded-lg"
-              name="email"
-              onChange={handleChange}
-              placeholder="Enter your email"
-            />
-  
-            <label htmlFor="password" className="ml-2">
-              Password
-            </label>
-        
-            <input
-              type={showPassword ? 'text' : 'password'}
-              className="p-2 border rounded-lg pr-10"
-              name="password"
-              onChange={handleChange}
-              placeholder="Enter your password"
-            />
-           
-        
-            <button
-              onClick={submit}
-              className="bg-[#074FB2] text-white py-2 rounded-lg mt-3 hover:bg-blue-600"
-            >
-              Login
-            </button>
-            <p>By continuing, you agree to our Conditions of Use and <span className="text-blue-600 cursor-pointer">Privacy Notice.</span> </p>
-          </form>
-  
-          <div className="flex justify-center items-center gap-4 mt-4">
-            <p className="text-[#074FB2] text-base">Don't have an account:</p>
-            <button
-              onClick={nav}
-              className="py-2 px-4 bg-white border rounded-lg text-sm hover:bg-slate-400"
-            >
-              Register
-            </button>
-          </div>
-  
-          <div className="flex justify-center items-center gap-4 mt-4">
-            <p className="text-[#074FB2] text-base">Back to home:</p>
-            <button
-              onClick={() => navigate("/")}
-              className="py-2 px-4 bg-white border rounded-lg text-sm hover:bg-slate-400"
-            >
-              Home
-            </button>
+      <div className="bg-gray-400 flex justify-center items-center min-h-screen ">
+        <div className="bg-gray-50 p-3 border rounded-xl shadow-xl max-w-3xl w-full sm:w-11/12 md:w-9/12 lg:w-8/12">
+          <h1 className="text-3xl text-blue-700 font-bold text-center">
+            Login Here
+          </h1>
+          <div className="flex flex-col md:flex-row">
+            <div className="w-full  px-5 mt-5">
+              <form className="flex flex-col gap-1">
+                <label htmlFor="email" className="ml-2">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="p-2 border rounded-lg"
+                  name="email"
+                  onChange={handleChange}
+                  placeholder="Enter your email"
+                />
+
+                <label htmlFor="password" className="ml-2">
+                  Password
+                </label>
+
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="p-2 border rounded-lg pr-10"
+                  name="password"
+                  onChange={handleChange}
+                  placeholder="Enter your password"
+                />
+
+                <button
+                  onClick={submit}
+                  className="bg-[#074FB2] text-white py-2 rounded-lg mt-3 hover:bg-blue-600"
+                >
+                  Login
+                </button>
+                <p>
+                  By continuing, you agree to our Conditions of Use and{" "}
+                  <span className="text-blue-600 cursor-pointer">
+                    Privacy Notice.
+                  </span>{" "}
+                </p>
+              </form>
+
+              <div className="flex justify-center items-center gap-4 mt-4">
+                <p className="text-[#074FB2] text-base">
+                  Don't have an account:
+                </p>
+                <button
+                  onClick={nav}
+                  className="py-2 px-4 bg-white border rounded-lg text-sm hover:bg-slate-400"
+                >
+                  Register
+                </button>
+              </div>
+
+              <div className="flex justify-center items-center gap-4 mt-4">
+                <p className="text-[#074FB2] text-base">Back to home:</p>
+                <button
+                  onClick={() => navigate("/")}
+                  className="py-2 px-4 bg-white border rounded-lg text-sm hover:bg-slate-400"
+                >
+                  Home
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  
     </>
   );
 };
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,10 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 
-
-const port =8000
-
-
 const Register = () => {
   const [input, setInput] = useState({
     name: "",
@@ -24,8 +20,8 @@ const Register = () => {
 
   const register = async (e) => {
     e.preventDefault();
-    const { name, email, password, cpassword } = input;
-    if (!name || !password || !email || !cpassword) {
+    const { name, email, password, password_confirmation } = input;
+    if (!name || !password || !email || !password_confirmation) {
       toast.warn("field missing", {
         position: "top-right",
         autoClose: 3000,
@@ -38,7 +34,7 @@ const Register = () => {
       });
       return false;
     }
-     if (!/^\S+@\S+\.\S+$/.test(email)) {
+    if (!/^\S+@\S+\.\S+$/.test(email)) {
       toast.error("Invalid email address", {
         position: "top-right",
         autoClose: 3000,
@@ -51,7 +47,7 @@ const Register = () => {
       });
       return false;
     }
-    if (password !== cpassword) {
+    if (password !== password_confirmation) {
       toast.error("password mismatch", {
         position: "top-right",
         autoClose: 3000,
@@ -64,7 +60,7 @@ const Register = () => {
       });
       return false;
     }
-    if (password.length < 6 ) {
+    if (password.length < 6) {
       toast.error("password must be 6 character long", {
         position: "top-right",
         autoClose: 3000,
@@ -80,25 +76,27 @@ const Register = () => {
     const passwordRegex = /(?=.*[A-Z])(?=.*[@#$%^&*!])(?=.*\d)/;
 
     if (!passwordRegex.test(password)) {
-      toast.error("Password must contain at least one uppercase letter, one special character, and one numeric character", {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
+      toast.error(
+        "Password must contain at least one uppercase letter, one special character, and one numeric character",
+        {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        }
+      );
       return false;
     }
-   
-    
-   
-    const response = await fetch(`http://localhost:${port}/api/register`, {
+
+
+    const response = await fetch(`http://127.0.0.1:8000/api/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, email, password, cpassword }),
+      body: JSON.stringify({ name, email, password, password_confirmation }),
     });
 
     const data = await response.json();
@@ -126,7 +124,7 @@ const Register = () => {
         progress: undefined,
         theme: "colored",
       });
-      nav("/Property");
+      nav("/");
     }
   };
   const navigate = useNavigate();
@@ -137,91 +135,95 @@ const Register = () => {
 
   return (
     <>
-    <div className="bg-gray-700 flex justify-center items-center min-h-screen">
+   <div className="bg-gray-700 flex justify-center items-center min-h-screen">
       <div className="bg-gray-100 p-3 border rounded-xl shadow-xl max-w-md w-full sm:w-10/12 md:w-8/12 lg:w-6/12">
-    <h2 className="text-3xl text-blue-700 font-bold text-center">
-      Register Yourself
-    </h2>
-    <div className="flex flex-col md:flex-row">
-    
-      <div className="w-full px-5 mt-5">
-        <form className="flex flex-col gap-1">
-          <label htmlFor="name" className="ml-2">
-            Name
-          </label>
-          <input
-            type="text"
-            className="p-2 border rounded-lg"
-            name="name"
-            placeholder="Enter your name"
-            onChange={handelChange}
-          />
-          <label htmlFor="email" className="ml-2">
-            Email
-          </label>
-          <input
-            type="email"
-            className="p-2 border rounded-lg"
-            name="email"
-            placeholder="Enter your email"
-            onChange={handelChange}
-          />
+        <h2 className="text-3xl text-blue-700 font-bold text-center">
+          Register Yourself
+          </h2>
+          <div className="flex flex-col md:flex-row">
+            <div className="w-full px-5 mt-5">
+              <form className="flex flex-col gap-1">
+                <label htmlFor="name" className="ml-2">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="p-2 border rounded-lg"
+                  name="name"
+                  placeholder="Enter your name"
+                  onChange={handelChange}
+                />
+                <label htmlFor="email" className="ml-2">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="p-2 border rounded-lg"
+                  name="email"
+                  placeholder="Enter your email"
+                  onChange={handelChange}
+                />
 
-          <label htmlFor="password" className="ml-2">
-            Password
-          </label>
-          <input
-            type="password"
-            className="p-2 border rounded-lg"
-            name="password"
-            placeholder="at least 6 character"
-            onChange={handelChange}
-          />
-          <p className="text-xs mb-2">  <i>!</i>  Passwords must be at least 6 characters. and there should one uppercase one numeric and a special character</p>
+                <label htmlFor="password" className="ml-2">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="p-2 border rounded-lg"
+                  name="password"
+                  placeholder="at least 6 character"
+                  onChange={handelChange}
+                />
+                <p className="text-xs mb-2">
+                  {" "}
+                  <i>!</i> Passwords must be at least 6 characters. and there
+                  should one uppercase one numeric and a special character
+                </p>
 
-          <label htmlFor="cpassword" className="ml-2">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            className="p-2 border rounded-lg"
-            name="cpassword"
-            placeholder="Confirm your password"
-            onChange={handelChange}
-          />
-    
-          <button
-            onClick={register}
-            className="bg-[#074FB2] text-white py-2 rounded-lg mt-3 hover:bg-blue-600"
-          >
-            Register
-          </button>
-        </form>
+                <label htmlFor="password_confirmation" className="ml-2">
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  className="p-2 border rounded-lg"
+                  name="password_confirmation"
+                  placeholder="Confirm your password"
+                  onChange={handelChange}
+                />
 
-        <div className="flex justify-center items-center gap-4 mt-4">
-          <p className="text-[#074FB2] text-base">Already have an account:</p>
-          <button
-            onClick={nav}
-            className="py-2 px-4 bg-white border rounded-lg text-sm hover:bg-slate-400"
-          >
-            Login
-          </button>
-        </div>
+                <button
+                  onClick={register}
+                  className="bg-[#074FB2] text-white py-2 rounded-lg mt-3 hover:bg-blue-600"
+                >
+                  Register
+                </button>
+              </form>
 
-        <div className="flex justify-center items-center gap-4 mt-4">
-          <p className="text-[#074FB2] text-base">Back to home:</p>
-          <button
-            onClick={() => navigate("/")}
-            className="py-2 px-4 bg-white border rounded-lg text-sm hover:bg-slate-400"
-          >
-            Home
-          </button>
+              <div className="flex justify-center items-center gap-4 mt-4">
+                <p className="text-[#074FB2] text-base">
+                  Already have an account:
+                </p>
+                <button
+                  onClick={nav}
+                  className="py-2 px-4 bg-white border rounded-lg text-sm hover:bg-slate-400"
+                >
+                  Login
+                </button>
+              </div>
+
+              <div className="flex justify-center items-center gap-4 mt-4">
+                <p className="text-[#074FB2] text-base">Back to home:</p>
+                <button
+                  onClick={() => navigate("/")}
+                  className="py-2 px-4 bg-white border rounded-lg text-sm hover:bg-slate-400"
+                >
+                  Home
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
-
     </>
   );
 };
