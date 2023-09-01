8728703f86cef6e50dd5a291d07891d9b392f99a
saswat <saswatr.mohanty@hyscaler.com>
2023-09-01 18:21:48 +0530

    complete

--- src/components/admin/ShowAllOrder.js
+++ src/components/admin/ShowAllOrder.js
@@ -14,16 +14,41 @@ const ShowAllOrder = () => {
       });
   }, []);
 
-  const handleOrder =async()=>{
-
+  const handleOrder = async (orderId) => {
+    try {
+      const response = await fetch(`http://localhost:8000/api/order-status/${orderId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+ 
+      });
+  console.log(response)
+      if (response.ok) {
+        setOrderedProducts((prevOrderedProducts) => {
+          const updatedProducts = prevOrderedProducts.map((product) => {
+            if (product.id === orderId) {
+              return { ...product, order_status: 'Delivered' }; 
+            }
+            return product;
+          });
+          return updatedProducts;
+        });
+      } else {
+        console.error('Error updating order status:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error updating order status:', error);
+    }
   };
+  
   return (
     <div className="container mx-auto py-8">
       <h2 className="text-2xl font-bold mb-4">All Ordered Products</h2>
       <ul className="space-y-4">
       {orderedProducts.map((product) => (
           <div>
-          <li key={product.id} className="bg-white shadow-md p-4 rounded-lg">
+          <li key={product.id+product.product_id} className="bg-white shadow-md p-4 rounded-lg">
             <div className="mb-2">
               <strong className="font-bold">Product ID:</strong>{" "}
               {product.product_id}
@@ -42,7 +67,7 @@ const ShowAllOrder = () => {
               </div>
             </div>
             <ProductDetails productId={product.product_id} />
-            <button onClick={handleOrder}  className="px-4 py-2 bg-slate-400 rounded-sm">Send Order </button>
+            <button onClick={()=>handleOrder(product.id)}  className="px-4 py-2 bg-slate-400 rounded-sm">Send Order </button>
             </li>
             </div>
             ))}
